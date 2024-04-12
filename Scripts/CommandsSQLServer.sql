55fdbe0b878b88fc062449534df91e25b3d540d0
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-04-12 11:07:15 -0300

    CommandsSQLServer.sql

---
+++ Scripts/CommandsSQLServer.sql
@@ -0,0 +1,47 @@
+# Comandos SQL Server
+
+Este repositório apresenta uma abordagem abrangente sobre os principais comandos SQL utilizados para a manipulação e gerenciamento de dados em um banco de dados relacional. Aqui você encontrará informações sobre os tipos de comandos SQL, sua sintaxe e exemplos práticos de utilização.
+
+## O que é SQL?
+
+SQL é uma linguagem de banco de dados projetada para a recuperação e gerenciamento de dados em um banco de dados relacional. É a linguagem padrão para o gerenciamento de bancos de dados, utilizada por sistemas como MySQL, SQL Server, Oracle, e outros.
+
+## Tipos de SQL
+
+1. **Data Definition Language (DDL)**: Define a estrutura do banco de dados.
+2. **Data Manipulation Language (DML)**: Permite modificar os dados no banco de dados.
+3. **Data Control Language (DCL)**: Gerencia os privilégios de acesso aos dados.
+4. **Transaction Control Language (TCL)**: Gerencia as transações no banco de dados.
+5. **Data Query Language (DQL)**: Permite a consulta de dados no banco de dados.
+
+## Principais Comandos
+
+### DDL (Data Definition Language)
+
+- **CREATE**: Define a estrutura do banco de dados.
+- **DROP**: Remove tabelas e bancos de dados.
+- **ALTER**: Altera a estrutura do banco de dados.
+- **TRUNCATE**: Remove todas as linhas de uma tabela.
+
+### DML (Data Manipulation Language)
+
+- **INSERT**: Insere novos dados em uma tabela.
+- **UPDATE**: Atualiza dados existentes em uma tabela.
+- **DELETE**: Remove dados de uma tabela.
+
+### DCL (Data Control Language)
+
+- **GRANT**: Concede privilégios de acesso a usuários.
+- **REVOKE**: Revoga privilégios de acesso de usuários.
+
+### TCL (Transaction Control Language)
+
+- **COMMIT**: Salva as alterações feitas em uma transação.
+- **ROLLBACK**: Desfaz alterações em uma transação não confirmada.
+- **SAVEPOINT**: Define um ponto de salvamento em uma transação.
+
+### DQL (Data Query Language)
+
+- **SELECT**: Recupera dados do banco de dados com base em condições especificadas.
+
+Para mais detalhes sobre cada comando e exemplos práticos de uso, confira a documentação e os exemplos disponíveis neste repositório.
